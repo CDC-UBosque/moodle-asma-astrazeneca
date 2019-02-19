0692696de1c1982716a070aec4a273608c38e220
Unknown <seguragabriel@unbosque.edu.co>
2019-02-19 08:49:52 -0500

    Work feb 19 (WIP)

---
+++ Menu/menu.js
@@ -0,0 +1,72 @@
+// Menu curso -----------------
+//
+$('#menu-asma').html('<div class="navbar"> <div class="navbar-inner"> <div class="container"> <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse-2"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </a> <a class="brand" href="https://ubosquemoodle.unbosque.edu.co/course/view.php?id=4227"> <div id="logo-top" class="logo"></div></a> <div class="nav-collapse collapse nav-collapse-2"> <nav class="menu"> <ul class="nav nav-pills"> <li><a href="https://ubosquemoodle.unbosque.edu.co/course/view.php?id=4227">Inicio</a></li><li class="dropdown timeit" data-start="2019-02-11 00:00"><a class="dropdown-toggle" data-toggle="dropdown">Módulo 0</a> <ul class="dropdown-menu"> <li><a href="#">Tema 1</a></li></ul> </li><li class="dropdown timeit" data-start="2019-02-11 00:00"><a class="dropdown-toggle" data-toggle="dropdown">Módulo 1</a> <ul class="dropdown-menu"> <li><a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=281624">Inicio</a></li><li><a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=281629">Etiología</a></li><li><a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=281630">Epidemiología</a></li><li><a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=281631">Aspectos de la Epidemiología</a></li><li><a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=281632">Fisiopatología</a></li><li><a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=281633">Promoción de la Salud</a></li><li><a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=281634">Bibliografía</a></li><li><a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=281724">Material del Módulo</a></li></ul> </li><li class="dropdown timeit" data-start="2019-02-11 00:00"><a class="dropdown-toggle" data-toggle="dropdown">Módulo 2</a> <ul class="dropdown-menu"> <li><a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=282215">Inicio</a></li><li><a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=282216">Manifestaciones clínicas</a></li><li><a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=282217">Algunas consideraciones en el paciente pediátrico</a></li><li><a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=282218">Clasificación</a></li><li><a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=282219">Medicamentos controladores</a></li><li><a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=282220">Medicamentos aliviadores</a></li><li><a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=282221">Detección precoz y tratamiento oportuno</a></li><li><a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=282222">Ejemplos prácticos</a></li><li><a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=282224">Bibliografía</a></li><li><a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=282649">Material de Módulo - presentación</a></li><li><a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=282650">Material de Módulo - casos clínicos</a></li></ul> </li><li class="dropdown timeit" data-start="2019-04-15 00:00"><a class="dropdown-toggle" data-toggle="dropdown">Módulo 3</a> <ul class="dropdown-menu"> <li><a href="#">Tema 1</a></li></ul> </li><li class="dropdown timeit" data-start="2019-04-29 00:00"><a class="dropdown-toggle" data-toggle="dropdown">Módulo 4</a> <ul class="dropdown-menu"> <li><a href="#">Tema 1</a></li></ul> </li></ul> </nav> </div></div></div></div>');
+
+// Logo astrazeneca -----------------
+//
+$('#logo-top').html('<img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/asma-astrazenca/img/svg/Logo-astrazeneca.svg" alt="AstraZeneca" />');
+
+// Footer logos -----------------
+//
+$('#footer-logos').html('<div class="footer"><div><img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/asma-astrazenca/img/svg/Logo-astrazeneca.svg" alt="AstraZeneca"/></div><div><img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/asma-astrazenca/img/svg/logo-UEB.svg" alt="Universidad El Bosque Especialización en Medicina Familiar"/></div><div><img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/asma-astrazenca/img/svg/logo-asma-cero.svg" alt="Asma Cero"/></div></div>');
+
+
+//
+//
+// Banners internos -----------------
+//
+//
+
+// Mod 0 -----------------
+//
+$('#mod0-1').html('<div class="mod-0"><div><h1>Módulo 0</h1><h1>Lineamientos</h1></div></div>');
+
+// Mod 1 -----------------
+//
+$('#mod1-1').html('<div class="mod-1"><div><h1>Módulo 1</h1><h1>Prevención Primaria</h1></div></div>');
+
+// Mod 2 -----------------
+//
+$('#mod2-1').html('<div class="mod-2"><div><h1>Módulo 2</h1><h1>Prevención Secundaria</h1></div></div>');
+
+
+
+//
+//
+// Progress bars -----------------
+//
+//
+// progreso 10%
+$('#progreso10').html('<div class="progress"><div class="bar" style="width: 10%;"></div></div>');
+// progreso 20%
+$('#progreso20').html('<div class="progress"><div class="bar" style="width: 20%;"></div></div>');
+// progreso 30%
+$('#progreso30').html('<div class="progress"><div class="bar" style="width: 30%;"></div></div>');
+// progreso 40%
+$('#progreso40').html('<div class="progress"><div class="bar" style="width: 40%;"></div></div>');
+// progreso 50%
+$('#progreso50').html('<div class="progress"><div class="bar" style="width: 50%;"></div></div>');
+// progreso 60%
+$('#progreso60').html('<div class="progress"><div class="bar" style="width: 60%;"></div></div>');
+// progreso 70%
+$('#progreso70').html('<div class="progress"><div class="bar" style="width: 70%;"></div></div>');
+// progreso 80%
+$('#progreso80').html('<div class="progress"><div class="bar" style="width: 80%;"></div></div>');
+// progreso 90%
+$('#progreso90').html('<div class="progress"><div class="bar" style="width: 90%;"></div></div>');
+// progreso 100%
+$('#progreso100').html('<div class="progress"><div class="bar" style="width: 100%;"></div></div>');
+
+
+
+
+//
+//
+// Module materials -----------------
+//
+//
+// Modulo 01
+$('#material01').html('<div> <h1>Material <span>del módulo en progreso</span></h1></div><ul class="thumbnails"> <li class="span4"> <div class="thumbnail"> <img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/asma-astrazenca/img/mod1/material01.jpg"> <div class="caption"> <p>Módulo 1 prevención primaria en asma</p><a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=250703">Ver Documento </a> </div></div></li></ul>');
+
+// Modulo 02
+$('#material02').html('<div> <h1>Material <span>del módulo en progreso</span></h1></div><ul class="thumbnails"> <li class="span4"> <div class="thumbnail"> <img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/asma-astrazenca/img/mod2/material02.jpg"> <div class="caption"> <p>Módulo 2 prevención secundaria</p><a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=282649">Ver Documento </a> </div></div></li> <li class="span4"> <div class="thumbnail"> <img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/asma-astrazenca/img/mod2/material01.jpg"> <div class="caption"> <p>Módulo 2 casos clínicos</p><a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=282650">Ver Documento </a> </div></div></li></ul>');
